0acfe30fb4e28765a44af217071a8402d6c06371
TomL1100 <tom.loudon.5@gmail.com>
2023-09-30 00:26:51 -0700

    Added toString

--- test_project/src/making_changes/Pont.java
+++ test_project/src/making_changes/Pont.java
@@ -8,4 +8,28 @@ public class Pont {
         this.x = x;
         this.y = y;
     }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Pont{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
