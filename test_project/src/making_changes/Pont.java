44305191956716b3197f72ee209cf4f56345ae91
TomL1100 <tom.loudon.5@gmail.com>
2023-09-29 17:42:22 -0700

    Added a pont class to represent (x,y) coordinates.

---
+++ test_project/src/making_changes/Pont.java
@@ -0,0 +1,11 @@
+package making_changes;
+
+public class Pont {
+    private double x;
+    private double y;
+
+    public Pont(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
